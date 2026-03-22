3afce71c7a4fb43a35a36e3c996814770cdc1896
contact1666 <contact1666@gmail.com>
2026-03-22 13:26:38 +0300

    fıxeddd

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__rounds=12)oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__rounds=12)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 # ── Veritabanı ──────────────────────────────────────────
 
